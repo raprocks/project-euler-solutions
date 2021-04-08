4b266548cc7d0397bc485b0824d3105cc9cb8d2f
Rohit Patil <rahulhimesh09@gmail.com>
2021-04-08 22:22:01 +0530

    [ADD] Solution for Problem 3
    Using `math.h` for `sqrt` function. Take a `long int` as input to the
    function and return a `int`. divide the number by 2 wholly and then
    divide it by each integer from 3 to square root of number skipping the
    even numbers in between. for loop and a nested if statement does the
    job. exit the loop as soon as number is 1 because then it does not have
    a prime factor left. Call the function in the main function and print
    the result.

---
+++ C/three.c
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+
+int max_prime_factor(long int num)
+{
+    int max_prime = 1;
+    int sqrt_num = sqrt(num);
+    while (num % 2 == 0) {
+        num /= 2;
+    }
+    for (long int i = 3; i < ((int)sqrt_num) + 1; i += 2) {
+        if (num % i == 0) {
+            max_prime = i;
+            while (num % i == 0) {
+                num /= i;
+            }
+        }
+        if (num == 1) {
+            break;
+        }
+    }
+    return max_prime;
+}
+
+int main()
+{
+    printf("%d\n", max_prime_factor(600851475143));
+    return 0;
+}
