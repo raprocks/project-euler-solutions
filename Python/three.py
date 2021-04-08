aaa5a6b542f4bcb1928663b8d0cdb45a3206cb9f
Rohit Patil <rahulhimesh09@gmail.com>
2021-04-08 22:28:39 +0530

    [PATCH] three.py's for loop
    was dividing by all even numbers now divides by 2 wholly and skips all
    even numbers and tries dividing by odd numbers only.

--- Python/three.py
+++ Python/three.py
@@ -2,7 +2,9 @@ import math
 
 def prime_max(num: int) -> int:
     max_prime: int = 1
-    for x in range(2, int(math.sqrt(num))+1, 2):
+    while num % 2 == 0:
+        num //= 2
+    for x in range(3, int(math.sqrt(num))+1, 2):
         if num % x == 0:
             max_prime = x
             while num % x == 0:
